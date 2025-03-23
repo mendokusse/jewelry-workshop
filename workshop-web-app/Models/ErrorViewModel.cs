a443ad86e9b3ee60de0466e996f557a39e61f7d1
mendokusse <anngnev@gmail.com>
2025-03-23 17:38:18 +0300

    feat(web): initial commit for ASP.NET web application

---
+++ workshop-web-app/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace workshop_web_app.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
