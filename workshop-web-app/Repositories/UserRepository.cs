aea2c2478320da4866202257fa6e9932a9e04250
mendokusse <anngnev@gmail.com>
2025-03-27 00:42:52 +0300

    feat(auth): add search user by email method

--- workshop-web-app/Repositories/UserRepository.cs
+++ workshop-web-app/Repositories/UserRepository.cs
@@ -233,5 +233,58 @@ namespace workshop_web_app.Repositories
 
             return affectedRows > 0;
         }
+
+        public async Task<List<User>> GetSearchUsersByEmailAsync(string email)
+        {
+            var users = new List<User>();
+
+            using (var connection = GetConnection())
+            {
+                await connection.OpenAsync();
+                string sql = @"
+                    SELECT 
+                        u.user_id, 
+                        u.role_id, 
+                        u.user_name, 
+                        u.user_password_hash, 
+                        u.user_phone, 
+                        u.user_email,
+                        r.role_id, 
+                        r.role_name
+                    FROM users u
+                    LEFT JOIN roles r ON u.role_id = r.role_id
+                    WHERE u.user_email ILIKE @email;
+                ";
+
+                using (var command = new NpgsqlCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("email", "%" + email + "%");
+
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var user = new User
+                            {
+                                UserId = reader.GetInt32(0),
+                                RoleId = reader.GetInt32(1),
+                                UserName = reader.GetString(2),
+                                UserPasswordHash = reader.GetString(3),
+                                UserPhone = reader.IsDBNull(4) ? null : reader.GetString(4),
+                                UserEmail = reader.IsDBNull(5) ? null : reader.GetString(5),
+                                Role = new Role
+                                {
+                                    RoleId = reader.GetInt32(6),
+                                    RoleName = reader.GetString(7)
+                                }
+                            };
+
+                            users.Add(user);
+                        }
+                    }
+                }
+            }
+            return users;
+        }
     }
 }
